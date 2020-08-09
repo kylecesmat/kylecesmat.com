cbf2788ff136e0fe40e11e4794e0ee4d6a0b201e
Kyle Cesmat <kyle.cesmat@formidable.com>
2020-08-09 16:33:51 -0700

    remove basepath for root domain deploymnet

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const isDev = process.env.NODE_ENV === 'development';
 
-module.exports = {
-  basePath: isDev ? '' : '/kylecesmat.com',
-  assetPrefix: isDev ? '' : '/kylecesmat.com/',
-};
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        speed: 3,
+        strip: true,
+        verbose: true,
+      },
+    },
+  ],
+]);
